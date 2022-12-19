bf4baec017bbd826596cd5c645519cf9a3f8f8cb
dev_toddler <99718527+DevToddler@users.noreply.github.com>
2022-12-20 04:41:00 +0900

    #6 - 스프링부트 프로젝트 시작하기
    들어간 기술
    * 액추에이터
    * 데브툴즈
    * 롬복
    * 스프링 웹

---
+++ src/main/java/com/bitstudy/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.bitstudy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
