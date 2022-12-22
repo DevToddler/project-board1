8e7cd6a8f4ef09c8e43d5c310fcd81ea610b0a22
dev_toddler <99718527+DevToddler@users.noreply.github.com>
2022-12-22 20:59:05 +0900

    #11 - repository interface
    repository interface

---
+++ src/main/java/com/bitstudy/app/repository/Ex05_ArticleCommentRepository_기본테스트용.java
@@ -0,0 +1,8 @@
+package com.bitstudy.app.repository;
+
+import com.bitstudy.app.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Ex05_ArticleCommentRepository_기본테스트용 extends JpaRepository<ArticleComment, Long> {
+
+}
