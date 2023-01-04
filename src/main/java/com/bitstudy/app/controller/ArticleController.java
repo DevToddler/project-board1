a4a206812032f00f93c6c02de161a902001de735
dev_toddler <99718527+DevToddler@users.noreply.github.com>
2023-01-04 17:52:43 +0900

    #18 - 게시판 뷰 기본 골격 작성1
    * 뷰 대략적인 뼈대 작성
        - templates > articles > index.html 파일 생성
    
    * 컨트롤러에 테스트를 통과할 수 있도록 매핑, 모델 세팅

---
+++ src/main/java/com/bitstudy/app/controller/ArticleController.java
@@ -0,0 +1,33 @@
+package com.bitstudy.app.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/** 뷰 엔드포인트 관련 컨트롤러
+ *
+ * 엑셀 api 에 정의해놓은 view 부분의 url.
+ 	/articles					GET 게시판 페이지
+  	/articles/{article-id}		GET 게시판 페이지
+  	/articles/search			GET 게시판 검색 전용 페이지
+  	/articles/search-hashtag	GET 게시판 해시태그 검색 전용 페이지
+ *
+ * Thymeleaf : 뷰 파일은 HTML 로 작업할건데, 타임리프를 설치 함으로서 스프링은 이제 HTML 파일을 마크업으로 보지 않고, 타임리프 템플릿 파일로 인식한다.
+ * 그래서 이 HTML 파일들은 아무데서나 작성할 수 없고, resources > templates 폴더 안에만 작성 가능하다.
+ * 그 외의 css, img, js 들은 resources > static 폴더 안에 작성 가능.
+ * */
+@Controller
+@RequestMapping("/articles") // 모든 경로들은 /articles 로 시작하니까 클래스 레벨에 1차로 @RequestMapping("/articles") 걸어 둠.
+public class ArticleController {
+
+	@GetMapping
+	public String articles(ModelMap map) {
+		/* Model 은 인터페이스, ModelMap 은 클래스(구현체) */
+		map.addAttribute("articles", List.of()); // 키 : articles, 값: 그냥 list
+		return "articles/index";
+	}
+}
